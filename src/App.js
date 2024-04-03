2e013f97d4d43bcf0bf9c6aaaf62acb5322b5111
VladislavMitkov <vladislavmitkovv@gmail.com>
2024-04-03 16:07:09 +0300

    Revert "Clean unnecessary code and dependencies"
    This reverts commit b9fcf75e2110c01d6e9aafcd0385046872b2cbc3.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <section>
+        <h2>Welcome to Task Trackr</h2>
+        <p>This is your planner application.</p>
+      </section>
+    </>
+  );
+};
+
+export default App;
