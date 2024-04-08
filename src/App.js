14c8275f751d9e602283222957be59d4356436ff
VladislavMitkov <vladislavmitkovv@gmail.com>
2024-04-08 22:04:47 +0300

    Access the endpoints

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from "react";
-
 const App = () => {
-  return <div>Hello there</div>;
+  const url = "http://localhost:5000";
+
+  const getAllTasks = () => {
+    fetch(url + "/tasks").then((res) => console.log(res.status));
+  };
+
+  const getTask = (id) => {
+    fetch(url + `/tasks/${id}`).then((res) => console.log(res.status));
+  };
+
+  const updateTask = (id) => {
+    fetch(url + `/tasks/${id}`).then((res) => console.log(res.status));
+  };
+  const deleteTask = (id) => {
+    fetch(url + `/tasks/${id}`).then((res) => console.log(res.status));
+  };
+
+  return (
+    <>
+      <div>Hello there</div>
+      <div>
+        <button onClick={getAllTasks}>Get all tasks</button>
+        <button onClick={() => getTask(4)}>Get single task</button>
+        <button onClick={() => updateTask(3)}>Update task</button>
+        <button onClick={() => deleteTask(3)}>Delete task</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
